6ceac4b43cb9b064caf2b6dd6e32e159b04acc6f
sreerag.k <sreerag.k@PIC4360>
2025-09-09 14:34:36 +0530

    Service unit test refactoring.

---
+++ Service.Tests/Fixture/SMSContextFixture.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SMS.Domain.Entities;
+using SMS.Infrastructure.Persistance;
+
+namespace Service.Tests.Fixture;
+public class SMSContextFixture : IAsyncLifetime
+{
+    public SMSContext? SMSContext { get; private set; } = null!;
+
+    public Task DisposeAsync()
+    {
+        if (SMSContext != null)
+            SMSContext.Dispose();
+
+        return Task.CompletedTask;
+    }
+
+    public Task InitializeAsync()
+    {
+        var options = new DbContextOptionsBuilder<SMSContext>()
+           .UseInMemoryDatabase(databaseName: "sms")
+           .Options;
+        SMSContext = new SMSContext(options, TimeProvider.System);
+        AddDummyStudents().ConfigureAwait(true);
+        return Task.CompletedTask;
+    }
+
+    private async Task AddDummyStudents()
+    {
+        SMSContext!.Students.Add(Student.Create(Guid.NewGuid(), "John", "M", new DateTime(1980, 12, 10), 'M', "john@test.com", "123456789", "test_address"));
+        await SMSContext!.SaveChangesAsync();
+    }
+}
--- Service.Tests/Tests/StudentServiceTests.cs
+++ Service.Tests/Tests/StudentServiceTests.cs
@@ -1,49 +1,35 @@
-﻿using Microsoft.EntityFrameworkCore;
-using SMS.Domain.Entities;
-using SMS.Infrastructure.Persistance;
-using SMS.Service.Common.Interface;
+﻿using Service.Tests.Fixture;
 using SMS.Service.Services.Students;
 
 namespace Service.Tests.Tests;
 
-public class StudentServiceTests
+public class StudentServiceTests : IClassFixture<SMSContextFixture>
 {
-    private ISMSContext _smsContext = null;
-    private IStudentService _studentService = null;
-    private Guid _id = Guid.NewGuid();
+    private IStudentService _studentService;
+    private readonly SMSContextFixture _fixture;
 
-    public StudentServiceTests()
+    public StudentServiceTests(SMSContextFixture fixture)
     {
-        var options = new DbContextOptionsBuilder<SMSContext>()
-            .UseInMemoryDatabase(databaseName: "sms")
-            .Options;
-        _smsContext = new SMSContext(options, TimeProvider.System);
-        _studentService = new StudentService(_smsContext, timeProvider: TimeProvider.System);
-
-        AddDummyStudents().ConfigureAwait(true);
-    }
-
-    [Fact]
-    public async Task GetByIdAsync_ReturnStudent()
-    {
-        var allStudnents = await _studentService.GetByIdAsync(_id);
-
-        Assert.NotNull(allStudnents);
-        Assert.Equal("John", allStudnents.FirstName);
+        _fixture = fixture;
+        _studentService = new StudentService(_fixture!.SMSContext, timeProvider: TimeProvider.System);
     }
 
     [Fact]
     public async Task GetAllAsync_ReturnDummyStudents()
     {
-        var allStudnents = await _studentService.GetAllAsync();
+        var allStudents = await _studentService.GetAllAsync();
 
-        Assert.NotNull(allStudnents);
-        Assert.Single(allStudnents);
+        Assert.NotNull(allStudents);
+        Assert.True(allStudents.Count() > 0);
     }
 
-    private async Task AddDummyStudents()
+    [Fact]
+    public async Task GetByIdAsync_ReturnStudent()
     {
-        _smsContext.Students.Add(Student.Create(_id, "John", "M", new DateTime(1980, 12, 10), 'M', "john@test.com", "123456789", "test_address"));
-        await _smsContext.SaveChangesAsync();
+        var firstStudent = (await _studentService.GetAllAsync()).First();
+        var student = await _studentService.GetByIdAsync(firstStudent.EntityId);
+        
+        Assert.NotNull(student);
+        Assert.Equal("John", student.FirstName);
     }
 }
